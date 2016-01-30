48f2fd5e067459a5be01e2d7e29a386e02048a42
Vincent Wu <wuv21@uw.edu>
2016-01-30 10:09:01 -0800

    added in algorithmia api code

--- js/script.js
+++ js/script.js
@@ -8,6 +8,36 @@ angular.module('BreadcrumbsApp', ['ui.router', 'ui.bootstrap', 'chart.js'])
             });
         $urlRouterProvider.otherwise('/main');
     })
-    .controller('MainController', function($scope, $state, $http, getUserData, InstaURL) {
-        console.log('here');
+    .controller('MainController', function($scope, $state, $http) {
+        $http.get('json/api_keys.json').success(function(data) {
+            var algo_key = data.config.algo;
+            $scope.client = Algorithmia.client(algo_key);
+
+            $scope.algoAPIs = {'html2text' : 'algo://util/Html2Text/0.1.3',
+                'summarizer' : 'algo://nlp/Summarizer/0.1.3',
+                'sentAnalysis' : 'algo://nlp/SentimentAnalysis/0.1.2'};
+
+            // overall fxn - prob will be obsolete due to asyncness
+            //$scope.algoQuery = function(algorithim, input) {
+            //    $scope.client.algo(algorithim)
+            //        .pipe(input)
+            //        .then(function(output) {
+            //            if (output.error) {
+            //                return console.error("error: " + output.error);
+            //            }
+            //            return output.result;
+            //        });
+            //};
+
+            $scope.getText = $scope.client.algo($scope.algoAPIs.html2text)
+                    .pipe("www.algorithmia.com")
+                    .then(function(output) {
+                        if (output.error) {
+                            return console.error("error: " + output.error);
+                        }
+                        console.log(output.result);
+                        return output.result;
+                    });
+        });
+
     });
